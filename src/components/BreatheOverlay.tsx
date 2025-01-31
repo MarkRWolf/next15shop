7bbfb850ebc85000c467df36d42187b2edaf87d7
igotzedxd <74432210+igotzedxd@users.noreply.github.com>
2025-01-31 02:15:30 +0100

    Update BreatheOverlay.tsx

--- src/components/BreatheOverlay.tsx
+++ src/components/BreatheOverlay.tsx
@@ -1,10 +1,11 @@
+"use client";
 import useNaviStore from "@/store/naviStore";
 
 const BreatheOverlay = () => {
   const isNavigating = useNaviStore((state) => state.isNavigating);
   return (
     <div
-      className={`fixed z-50 inset-0 transition-opacity duration-300 bg-gradient-to-b from-gray-100 to-gray-200 ${isNavigating ? "opacity-50" : "opacity-0"} pointer-events-none`}
+      className={`fixed w-full h-screen z-50 inset-0 transition-opacity duration-300 bg-gradient-to-b from-gray-100 to-gray-200 ${isNavigating ? "opacity-50" : "opacity-0"} pointer-events-none`}
     />
   );
 };
