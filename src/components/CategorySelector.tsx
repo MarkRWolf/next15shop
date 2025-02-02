e20d5c29ea00585ce85f4eb6b2edc0536720162e
igotzedxd <74432210+igotzedxd@users.noreply.github.com>
2025-02-02 03:26:51 +0100

    hmm

--- src/components/CategorySelector.tsx
+++ src/components/CategorySelector.tsx
@@ -110,6 +110,24 @@ const CategorySelector = ({ categories, categoryTexts }: CategorySelectorProps) 
                   </BetterLink>
                 </span>
               </CommandItem>
+              <CommandItem asChild>
+                <span
+                  onClick={() => {
+                    setValue("xdd");
+                    setOpen(false);
+                  }}
+                >
+                  <BetterLink href={`/`} className="flex w-full items-center justify-between">
+                    Products
+                    <Check
+                      className={cn(
+                        "ml-auto h-4 w-4",
+                        value === "xdd" ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                  </BetterLink>
+                </span>
+              </CommandItem>
             </CommandGroup>
           </CommandList>
         </Command>
