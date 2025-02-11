9d433733e64332e1f04c1ddd1c79e97a4af8026e
igotzedxd <74432210+igotzedxd@users.noreply.github.com>
2025-02-11 23:34:07 +0100

    fixed hero pagination

--- src/components/saleSlider/SalePagination.tsx
+++ src/components/saleSlider/SalePagination.tsx
@@ -4,11 +4,11 @@ import { useSwiper } from "swiper/react";
 
 const SalePagination = ({ sales, heroes }: { sales: Sale[]; heroes: Hero[] }) => {
   const swiper: Swiper = useSwiper();
-    const allSlides = [...sales, ...heroes];
-    
+  const allSlides = [...sales, ...heroes];
+
   return (
     <div className="absolute bottom-0 left-0 right-0 flex justify-center z-10 text-white gap-2">
-      {sales.map((sale, index) => (
+      {allSlides.map((sale, index) => (
         <div
           key={sale._id}
           className="flex justify-center items-center p-1 "
