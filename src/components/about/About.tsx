3b3db279aeb655bc16b9c05128842a00f47503ed
igotzedxd <74432210+igotzedxd@users.noreply.github.com>
2025-02-22 01:19:20 +0100

    added secTitle for example section

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -11,6 +11,7 @@ interface AboutProps {
 const About = ({ aboutText, examplesText }: AboutProps) => {
   const about = useText(aboutText, "chap", "chapters");
   const examples = useText(examplesText, "exampleTitle", "chapters");
+  const secTitle = useText(examplesText, "secTitle", "single");
 
   return about?.length ? (
     <div className="container-main w-full px-2 sm:px-0">
@@ -26,11 +27,12 @@ const About = ({ aboutText, examplesText }: AboutProps) => {
             </div>
           )
         )}
-        <div className="space-y-12">
+        <div className="space-y-12 text-3xl pt-4">
+          {secTitle}
           {examples?.length
             ? examples.map((ex, index) => (
-                <div key={index} className="space-y-4">
-                  <h2 className="font-semibold text-lg">{ex}</h2>
+                <div key={index} className="space-y-4 first:mt-4">
+                  <h2 className="text-xl">{ex}</h2>
                   <video controls className="w-full" muted>
                     <source src={`/example_${index + 1}.mp4`} type="video/mp4" />
                     Your browser does not support the video tag.
